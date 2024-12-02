e5f2f6727406195041e228676154f62f93af5ffd
edisxon <edisxon10@gmail.com>
2024-12-02 14:54:27 -0500

    falta mejorar el crud de author para que cambie el rol de user

--- controllers/author/delete.js
+++ controllers/author/delete.js
@@ -1,18 +1,18 @@
-import User from "../../models/User.js"
+import Author from "../../models/Author.js";
 
 let deleteOne = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const deleted = await User.findByIdAndDelete(id);
+    const deleted = await Author.findByIdAndDelete(id);
     if (!deleted) {
       return res.status(404).json({
         success: false,
-        message: "User not found",
+        message: "Author not found",
       });
     }
     return res.status(200).json({
       success: true,
-      message: "User successfully deleted",
+      message: "Author successfully deleted",
       response: deleted,
     });
   } catch (error) {
--- router/author.js
+++ router/author.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
 import { allAuthors,getAuthorById,searchAuthorsByName } from "../controllers/author/read.js";
+import { updatePhoto } from "../controllers/author/update.js";
+import { create } from "../controllers/author/create.js";
+import { deleteOne } from "../controllers/author/delete.js";
 
 
 
@@ -10,6 +13,10 @@ const router = Router();
 
 router.get("/all", allAuthors)
 router.get('/:id',getAuthorById)
+router.post("/create", create)
+router.put("/updatePhoto", updatePhoto)
+router.delete("/deleteOne", deleteOne)
+
 
 
 
