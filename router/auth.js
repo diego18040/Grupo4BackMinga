489e108aad76ab4bf3202a8027e9bc22ab8bf7a9
gabriel <gabriel199777@outlook.com>
2024-12-03 19:25:18 -0300

    feat: agregamos shema :signup , userUpdate. controlador auth - inicio secion por google

--- router/auth.js
+++ router/auth.js
@@ -5,12 +5,26 @@ import accaountNoExist from "../middleware/accountNoExits.js"
 import isValidadPassword from "../middleware/isValidadPassword.js"
 import generateToken from "../middleware/generateToken.js"
 import passport from "../middleware/passport.js"; 
+import passportGoogle from "../middleware/passportGoogle.js";
+import {handleGoogleCallback} from "../controllers/auth/handleGoogleCallback.js"
 
 
 const routerAuth = Router();
 
 routerAuth.post("/signIn",accaountNoExist,isValidadPassword,generateToken,signIn)
 routerAuth.post("/signout", passport.authenticate("jwt", { session: false }), signOut)
+//ruta para iniciar secion con google
+routerAuth.get("/signin/google",passportGoogle.authenticate("google",{session:false, scope:["profile","email"]})
+)
+// Ruta de callback después de la autenticación con Google
+routerAuth.get(
+    '/signin/google/callback',
+    passport.authenticate('google', {
+      session: false,
+      failureRedirect: `${process.env.FRONTEND_URL || 'http://localhost:5173'}/signin`,
+    }),
+    handleGoogleCallback // Controlador para manejar el callback
+  );
 
 
 
