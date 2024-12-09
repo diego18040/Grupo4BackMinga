ea6b0d08a114532b5dee317bc11ee0cc1d9f695c
Felipe Quesada <felquev@mail.com>
2024-12-08 23:39:43 -0600

    aa

--- router/user.js
+++ router/user.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { allUsers,userById } from "../controllers/user/read.js";
+import { allUsers, userById, userByIdToken } from "../controllers/user/read.js";
 import { register } from "../controllers/user/resgister.js";
-import  update  from "../controllers/user/update.js";
+import update from "../controllers/user/update.js";
 import { deleteOne } from "../controllers/user/delete.js";
 import passport from "../middleware/passport.js";
 import accountExists from "../middleware/accountExist.js";
@@ -12,13 +12,15 @@ import userUpdateSchema from "../schema/updateUsers.js"
 
 
 
+
 const routerUsers = Router();
 
-routerUsers.get("/all",passport.authenticate('jwt',{session:false}),allUsers)
-routerUsers.get('/:id',passport.authenticate('jwt',{session:false}),userById)
-routerUsers.post("/register",validator(userSignUpSchema),accountExists,createHash, register)
-routerUsers.put("/:id",validator(userUpdateSchema),passport.authenticate('jwt',{session:false}),createHash, update)
-routerUsers.delete("/deleteOne/:id",passport.authenticate('jwt',{session:false}), deleteOne)
+routerUsers.get("/all", passport.authenticate('jwt', { session: false }), allUsers)
+routerUsers.get('/:id', passport.authenticate('jwt', { session: false }), userById)
+routerUsers.post("/register", validator(userSignUpSchema), accountExists, createHash, register)
+routerUsers.put("/:id", validator(userUpdateSchema), passport.authenticate('jwt', { session: false }), createHash, update)
+routerUsers.delete("/deleteOne/:id", passport.authenticate('jwt', { session: false }), deleteOne)
+routerUsers.get('/validatetoken', passport.authenticate('jwt', { session: false }), userByIdToken);
 
 
 
