5f1ecd4ec95578d74299c36be6471267a7a8e807
edisxon <edisxon10@gmail.com>
2024-12-09 18:48:36 -0500

    :passport_control: añadido middleware de rol en ciertas rutas

--- router/comment.js
+++ router/comment.js
@@ -4,15 +4,16 @@ import { updateMessage } from "../controllers/comments/update.js";
 import { deleteOne } from "../controllers/comments/delete.js";
 import { create } from "../controllers/comments/create.js";
 import passport from "../middleware/passport.js";
+import isRole0 from "../middleware/isRole0.js";
 
 const router = Router();
 
 
 router.get("/all", passport.authenticate("jwt", { session: false }), allComments)
 router.get("/chapter/:id", passport.authenticate("jwt", { session: false }), commentsByChapterId)
-router.put("/updateMessage/:id", passport.authenticate("jwt", { session: false }), updateMessage)
+router.put("/updateMessage/:id", passport.authenticate("jwt", { session: false }), isRole0, updateMessage)
 router.delete("/deleteOne/:id", passport.authenticate("jwt", { session: false }), deleteOne)
-router.post("/create", passport.authenticate("jwt", { session: false }), create)
+router.post("/create", passport.authenticate("jwt", { session: false }), isRole0, create)
 
 
 
